d82562883ad82b1c89fd76d3e9aaf7903841ace8
Noah Berman <noah@noahberman.org>
2016-08-15 16:05:11 -0600

    just some garbage code

--- credit_check.rb
+++ credit_check.rb
@@ -4,6 +4,23 @@ valid = false
 
 # Your Luhn Algorithm Here
 
+# double the value of every second digit moving right - > left
+
+def card_check(card_number)
+  numbers = card_number.split("")
+  
+
+
+# sum (or subtract 9 from) the digits of anything over 9
+
+# sum all the digits
+
+# iff the total modulo 10 is equal to 0 then the number is valid
+
+
+
+
+
 # Output
 ## If it is valid, print "The number is valid!"
 ## If it is invalid, print "The number is invalid!"
