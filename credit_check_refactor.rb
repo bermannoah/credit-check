a9c062819dbd990e911846b9adc40a76cbb720c2
Noah Berman <noah@noahberman.org>
2016-08-17 14:28:42 -0600

    minor refactor

--- credit_check_refactor.rb
+++ credit_check_refactor.rb
@@ -3,14 +3,14 @@ invalid_card_number = "4024007106512380"
 
 # split the string into individual characeters
 # convert stringed digits to integers
-usable_array = invalid_card_number.chars.map { |e| e.to_i }
+usable_array = valid_card_number.chars.map { |num| num.to_i }
 # double the value of every second digit
-numbers_to_multiply = usable_array.map.with_index do |x, index|
-  index.even? ? x * 2 : x
+numbers_to_multiply = usable_array.map.with_index do |num, index|
+  index.even? ? num * 2 : num
 end
 # if the digit is over 9, add the 2 numbers together
-final_numbers = numbers_to_multiply.map do |x|
-  x >= 10 ? x - 9 : x
+final_numbers = numbers_to_multiply.map do |multiplied|
+  multiplied >= 10 ? multiplied - 9 : multiplied
 end
 # take the sum of the remaining digits
 sum = final_numbers.inject(:+)
